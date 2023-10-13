d79b1c4e0a6d4fcfc9a6ca6939421bac651ed3ed
Caroline Wang <cxw320@case.edu>
2023-10-13 15:12:59 -0400

    add dto for recipe object

--- lib/data/dto/recipe_dto.dart
+++ lib/data/dto/recipe_dto.dart
@@ -1,8 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
 import 'package:recipe_finder/domain/entity/recipe.dart';
 
+part 'recipe_dto.g.dart';
+
+@JsonSerializable()
 class RecipeDto extends Recipe {
   RecipeDto(this.id, this.title, this.image) : super(id: id);
   final int id;
   final String? title;
   final String? image;
+
+  factory RecipeDto.fromJson(Map<String, dynamic> json) =>
+      _$RecipeDtoFromJson(json);
+  Map<String, dynamic> toJson() => _$RecipeDtoToJson(this);
 }
--- lib/data/source/network/spoonacular_api.dart
+++ lib/data/source/network/spoonacular_api.dart
@@ -1,23 +1,35 @@
 // Step 1: Create abstract class (this is good when we need to mock this for testing)
 
 import 'package:dio/dio.dart';
+import 'package:recipe_finder/data/dto/recipe_dto.dart';
 
 abstract class SpoonacularApi {
   final Dio dio;
   SpoonacularApi({required this.dio});
 
   //Create a method for retrieving random recipes
-  Future<List<RecipeDto> getRandomRecipes();
-
+  Future<List<RecipeDto>> getRandomRecipes();
 }
 
 // Step 2: Create implementation class
 
 class SpoonacularApiImpl implements SpoonacularApi {
+  final Dio dio;
+
+  SpoonacularApiImpl({required this.dio});
+
   @override
   // TODO: implement dio
-  Dio get dio => throw UnimplementedError();
-
-
-
+  Future<List<RecipeDto>> getRandomRecipes() async {
+    try {
+      Response response = await dio.get("");
+      final recipeListResponse = (response.data["recipes"] as List)
+          .map((response) => RecipeDto.fromJson(response))
+          .toList();
+      return Future.value(recipeListResponse);
+    } on DioException catch (error) {
+      print(error);
+    }
+    return [];
+  }
 }
