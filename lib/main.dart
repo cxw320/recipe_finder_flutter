ec81e98915f2489348ebaab5f104011f328d6a83
Caroline Wang <cxw320@case.edu>
2023-10-15 14:28:26 -0400

    create examples of stateless and statefulwidgets

--- lib/main.dart
+++ lib/main.dart
@@ -7,61 +7,43 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'My App',
-      home: HomeScreen(
-        title: 'My title',
+      home: Scaffold(
+        body: Center(
+          child: MyCounter(),
+        ),
       ),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key, required this.title});
-
-  final String title;
+class MyCounter extends StatefulWidget {
+  const MyCounter({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<MyCounter> createState() {
+    return _MyCounterState();
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _MyCounterState extends State<MyCounter> {
   int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    setState(
+      () => _counter++,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
+    return Center(
+      child: ElevatedButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Text('$_counter'),
+      ),
     );
   }
 }
